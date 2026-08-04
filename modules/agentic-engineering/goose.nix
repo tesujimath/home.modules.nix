e1bd5d14500300441ca965995b5f1c6c2e3430ae
Simon Guest <simon.guest@datacom.com>
2026-08-04 17:22:32 +1200

    Remove goose providers
    They have no business being public

--- modules/agentic-engineering/goose.nix
+++ modules/agentic-engineering/goose.nix
@@ -23,46 +23,14 @@ in
                 ${pkgs.goose-cli}/bin/goose completion fish > $out
               '';
           } else { };
-
-        providers = {
-          "goose/custom_providers/oMLX.json".text = builtins.toJSON {
-            name = "oMLX";
-            engine = "openai";
-            display_name = "Local oMLX";
-            description = "oMLX running on localhost";
-            base_url = "http://localhost:8000/v1";
-            models = [
-              {
-                name = "Devstral-Small-2-24B-Instruct-2512-4bit";
-                context_limit = 393216;
-              }
-              {
-                name = "Ornith-1.0-9B-4bit";
-                context_limit = 262144;
-              }
-              {
-                name = "Ornith-1.0-35B-5bit-XL-mlx";
-                context_limit = 262144;
-              }
-              {
-                name = "Qwen3.6-27B-MLX-4bit";
-                context_limit = 262144;
-              }
-            ];
-          };
-        };
       in
-      completions // providers;
+      completions;
 
     home =
       {
         packages = with pkgs; [
           goose-cli
         ];
-
-        sessionVariables = {
-          GOOSE_PROVIDER = "oMLX";
-        };
       };
   };
 }
