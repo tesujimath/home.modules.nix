247aa594b0e04e761f70c75e8e6e902332db8c0a
tesujimath <tesujimath@users.noreply.github.com>
2026-09-15 06:30:58 +0000

    deploy: 0cbac79254c9c0ba68fc65e89011f20cfb9183df

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9c3f8dc3.js',
+                path_to_root + 'searchindex-493be408.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
