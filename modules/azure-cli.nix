a85817ed2e2b3d412bed9206f5b5341b8e340c93
Simon Guest <simon.guest@datacom.com>
2026-09-22 09:07:41 +1200

    Add log-analytics as a default extension for azure-cli

--- modules/azure-cli.nix
+++ modules/azure-cli.nix
@@ -10,19 +10,22 @@ in
     extensions = mkOption {
       type = lib.types.listOf lib.types.package;
       description = "Azure-CLI extension to install";
-      default =
+      default = with pkgs.azure-cli-extensions;
         let
-          # latest version required to list pipelines 🤷
-          azure-devops = pkgs.azure-cli-extensions.azure-devops.overridePythonAttrs (_: rec {
-            version = "1.0.8";
-            src = pkgs.fetchurl {
-              url = "https://github.com/Azure/azure-cli-extensions/releases/download/azure-devops-${version}/azure_devops-${version}-py2.py3-none-any.whl";
-              hash = "sha256-uQzJx2TLZrBj6LXUBxY/PnqUS7UM9d18SC9K4Zj5Nk4=";
-            };
-          });
+          my = {
+            # latest version required to list pipelines 🤷
+            azure-devops = azure-devops.overridePythonAttrs (_: rec {
+              version = "1.0.8";
+              src = pkgs.fetchurl {
+                url = "https://github.com/Azure/azure-cli-extensions/releases/download/azure-devops-${version}/azure_devops-${version}-py2.py3-none-any.whl";
+                hash = "sha256-uQzJx2TLZrBj6LXUBxY/PnqUS7UM9d18SC9K4Zj5Nk4=";
+              };
+            });
+          };
         in
         [
-          azure-devops
+          my.azure-devops
+          log-analytics
         ];
     };
 
